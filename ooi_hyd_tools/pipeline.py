c7ecccdce462132260fb075aa98da43055614aff
jdduprey <jduprey@uw.edu>
2025-03-07 13:49:36 -0800

    option to run in parallel with prefect in cloud

--- ooi_hyd_tools/pipeline.py
+++ ooi_hyd_tools/pipeline.py
@@ -1,9 +1,11 @@
 import click
 
-from prefect import flow, task
+from prefect.deployments import run_deployment
 from datetime import datetime, timedelta
 from ooi_hyd_tools.mseed_to_audio import acoustic_flow_oneday
+from loguru import logger
 
+PREFECT_DEPLOYMENT = "hydbb_pipeline_2vcpu_16gb"
 
 @click.command()
 @click.option(
@@ -79,6 +81,13 @@ from ooi_hyd_tools.mseed_to_audio import acoustic_flow_oneday
     show_default=True, 
     help="Which stage of pipeline to run: 'audio' converts mseed to audio, 'viz' converts audio to spectrograms, 'all' runs both."
 )
+@click.option(
+    "--parallel-in-cloud",
+    type=bool,
+    default=False,
+    show_default=True,
+    help="run prefect deployment in parellel in cloud, parallelized by date - need access to RCA cloud"
+)
 def run_acoustic_pipeline(
     start_date,
     end_date,
@@ -91,41 +100,95 @@ def run_acoustic_pipeline(
     apply_cals,
     s3_sync,
     stages,
+    parallel_in_cloud,
 ):
-    start_date = datetime.strptime(start_date, "%Y/%m/%d")
+    
+    if parallel_in_cloud:
 
-    if end_date is None: # run a single day
-         acoustic_flow_oneday(
-                hyd_refdes=hyd_refdes,
-                date=start_date.strftime("%Y/%m/%d"),
-                sr=sr,
-                format=format,
-                normalize_traces=normalize_traces,
-                fudge_factor=fudge_factor,
-                write_wav=write_wav,
-                apply_cals=apply_cals,
-                s3_sync=s3_sync,
-                stages=stages,
+        start_date = datetime.strptime(start_date, "%Y/%m/%d")
+        if end_date is None:
+
+            run_name = f"{hyd_refdes}_{start_date}"
+            params = {
+                "hyd_refdes": hyd_refdes,
+                "date": start_date.strftime("%Y/%m/%d"),
+                "sr": sr,
+                "format": format,
+                "normalize_traces": normalize_traces,
+                "fudge_factor": fudge_factor,
+                "write_wav": write_wav,
+                "apply_cals": apply_cals,
+                "s3_sync": s3_sync,
+                "stages": stages,
+            }
+
+            logger.info(f"Launching workflow for {run_name} in cloud")
+            run_deployment(
+                name=PREFECT_DEPLOYMENT,
+                parameters=params,
+                flow_run_name=run_name,
+                timeout=5
             )
+        else: 
+            end_date = datetime.strptime(end_date, "%Y/%m/%d")
+            while start_date <= end_date:
+                run_name = f"{hyd_refdes}_{start_date}"
+                params = {
+                    "hyd_refdes": hyd_refdes,
+                    "date": start_date.strftime("%Y/%m/%d"),
+                    "sr": sr,
+                    "format": format,
+                    "normalize_traces": normalize_traces,
+                    "fudge_factor": fudge_factor,
+                    "write_wav": write_wav,
+                    "apply_cals": apply_cals,
+                    "s3_sync": s3_sync,
+                    "stages": stages,
+                }
+                logger.info(f"Launching workflow for {run_name} in cloud")
+                run_deployment(
+                name=PREFECT_DEPLOYMENT,
+                parameters=params,
+                flow_run_name=run_name,
+                timeout=5
+                )
+                start_date += timedelta(days=1)
+    
+    else:
+        start_date = datetime.strptime(start_date, "%Y/%m/%d")
 
-    else: # run a range of days
-        end_date = datetime.strptime(end_date, "%Y/%m/%d")
-
-        while start_date <= end_date:
+        if end_date is None: # run a single day
             acoustic_flow_oneday(
-                hyd_refdes=hyd_refdes,
-                date=start_date.strftime("%Y/%m/%d"),
-                sr=sr,
-                format=format,
-                normalize_traces=normalize_traces,
-                fudge_factor=fudge_factor,
-                write_wav=write_wav,
-                apply_cals=apply_cals,
-                s3_sync=s3_sync,
-                stages=stages,
-            )
+                    hyd_refdes=hyd_refdes,
+                    date=start_date.strftime("%Y/%m/%d"),
+                    sr=sr,
+                    format=format,
+                    normalize_traces=normalize_traces,
+                    fudge_factor=fudge_factor,
+                    write_wav=write_wav,
+                    apply_cals=apply_cals,
+                    s3_sync=s3_sync,
+                    stages=stages,
+                )
 
-            start_date += timedelta(days=1)
+        else: # run a range of days
+            end_date = datetime.strptime(end_date, "%Y/%m/%d")
+
+            while start_date <= end_date:
+                acoustic_flow_oneday(
+                    hyd_refdes=hyd_refdes,
+                    date=start_date.strftime("%Y/%m/%d"),
+                    sr=sr,
+                    format=format,
+                    normalize_traces=normalize_traces,
+                    fudge_factor=fudge_factor,
+                    write_wav=write_wav,
+                    apply_cals=apply_cals,
+                    s3_sync=s3_sync,
+                    stages=stages,
+                )
+
+                start_date += timedelta(days=1)
 
 
 # local debugging
