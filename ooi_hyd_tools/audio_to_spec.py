0f092889bf354034128acda6caea5e85587ebe3d
jdduprey <jduprey@uw.edu>
2025-03-11 09:57:06 -0700

    don't apply cals yet

--- ooi_hyd_tools/audio_to_spec.py
+++ ooi_hyd_tools/audio_to_spec.py
@@ -108,9 +108,9 @@ def gen_hybrid_millidecade_spectrogram(start_date, instrument, apply_cals=False)
     hmb_gen.set_subset_to(FREQ_LIMS)
 
     if apply_cals: #TODO
-        logger.warning("APPLYING CONSTANT CALL")
+        logger.warning("CALLS NOT YET IMPLEMENTED")
         #sensitivity_uri =  "./metadata/cals/NRS11_H5R6_sensitivity_hms5kHz_PLACEHOLDER.nc"
-        hmb_gen.set_sensitivity(-170) 
+        #hmb_gen.set_sensitivity(-170) 
         #hmb_gen.set_sensitivity(sensitivity_uri)
 
     config = Config(signature_version=botocore.UNSIGNED)
